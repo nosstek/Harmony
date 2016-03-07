bd8a8f33db324a54bb0030624d9e1aca3a5a837a
Lukasz Jaworski <200536@student.pwr.edu.pl>
2016-03-07 18:18:24 +0100

    Stworzenie struktury projektu i podstawowa konfiguracja

---
+++ HarmonyWebApp/HarmonyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HarmonyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HarmonyWebApp/HarmonyWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HarmonyWebApp.MvcApplication" Language="C#" %>
